3eb4fdd20f70c35c71adb313b045edc1a8f7229c
Sowmya Paluri <92660924+SowmyaPaluri@users.noreply.github.com>
2024-12-23 23:13:18 +0530

    Update and rename powertest.java to PowerTest.java

---
+++ src/test/PowerTest.java
@@ -0,0 +1,23 @@
+package powerpackage;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class PowerTest{
+    @Test
+    public void testOneRaisedToOneIsOne(){
+        assertEquals(1, PowerFinder.calculatePower(1, 1));
+    }
+    @Test
+    public void testTwoRaisedToOneIsTwo(){
+        assertEquals(2, PowerFinder.calculatePower(2, 1));
+    }
+    @Test
+    public void testTwoRaisedToTwoIsFour(){
+        assertEquals(4, PowerFinder.calculatePower(2, 2));
+    }
+    @Test
+    public void testPowerOfThreeToTwoIsNine(){
+        assertEquals(9, PowerFinder.calculatePower(3, 2));
+    }
+}
